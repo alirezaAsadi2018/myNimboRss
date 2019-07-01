64d18122d392bb6255feb57f62d601a28645c5e8
alireza <asday529@gmail.com>
2019-07-01 15:01:00 +0430

    reading RSS

--- src/main/java/in/nimbo/App.java
+++ src/main/java/in/nimbo/App.java
@@ -8,14 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         RSSFeedParser parser = new RSSFeedParser("https://www.tabnak.ir/fa/rss/allnews");
         Feed feed = parser.readFeed();
-//        System.out.println(feed.getTitle());
         for (FeedMessage message : feed.getMessages()) {
+            System.out.print(message.title + " ");
             System.out.println(message.title);
-//            System.out.println(message);
         }
-//        System.out.println(feed.getMessages());
     }
 }
